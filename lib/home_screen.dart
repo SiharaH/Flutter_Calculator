173091f6595fb6d8898b7c6ad11be4da07ba731f
KHSH Edirisinghe <siharaedirisinghe03@gmail.com>
2026-04-09 15:19:36 +0530

    Implement the calculator functionality

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,13 +9,183 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String userInput = "";
+  String result = "0";
+
+  List<String> buttonList = [
+    "AC",
+    "(",
+    ")",
+    "/",
+    "7",
+    "8",
+    "9",
+    "*",
+    "4",
+    "5",
+    "6",
+    "+",
+    "1",
+    "2",
+    "3",
+    "-",
+    "C",
+    "0",
+    ".",
+    "=",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(children: [],
+        body: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 2.90,
+              child: resultWidget(),
+            ),
+            Expanded(child: buttonWidget()),
+          ],
+        ),
       ),
-      )
-    ); 
+    );
+  }
+
+  Widget resultWidget() {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.all(20),
+            child: Text(userInput, style: TextStyle(fontSize: 32)),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.all(10),
+            child: Text(
+              result,
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buttonWidget() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      color: Color.fromARGB(66, 233, 232, 232),
+      child: GridView.builder(
+        itemCount: buttonList.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (context, index) {
+          return button(buttonList[index]);
+        },
+      ),
+    );
+  }
+
+  getColor(String text) {
+    if (text == "/" ||
+        text == "*" ||
+        text == "+" ||
+        text == "-" ||
+        text == "C" ||
+        text == "(" ||
+        text == ")") {
+      return Colors.redAccent;
+    }
+    if (text == "=" || text == "AC") {
+      return Colors.white;
+    }
+    return Colors.indigo;
+  }
+
+  getBgColor(String text) {
+    if (text == "AC") {
+      return Colors.redAccent;
+    }
+    if (text == "=") {
+      return Color.fromARGB(255, 104, 204, 159);
+    }
+    return Colors.white;
+  }
+
+  Widget button(String text) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          handleButtonPress(text);
+        });
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: getBgColor(text),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              blurRadius: 1,
+              spreadRadius: 1,
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: getColor(text),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  handleButtonPress(String text) {
+    if (text == "AC") {
+      userInput = "";
+      result = "0";
+      return;
+    }
+    if (text == "C") {
+      if (userInput.length > 0) {
+        userInput = userInput.substring(0, userInput.length - 1);
+        return;
+      } else {
+        return null;
+      }
+    }
+
+    if (text == "=") {
+      result = calculate();
+      userInput = result;
+      if (result.endsWith(".0")) {
+        result = result.replaceAll(".0", "");
+      }
+      return;
+    }
+    userInput += text;
+  }
+
+  String calculate() {
+    try {
+      var exp = Parser().parse(userInput);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return evaluation.toString();
+    } catch (e) {
+      return "Error";
+    }
   }
 }
